2b085eb3036e71ce37493a45c14fe2658eef0cae
Sander van Zoest <svanzoest@onehealth.com>
2013-06-06 19:55:36 -0700

    more explicitly define operating systems and conflicting community cookbooks

--- metadata.rb
+++ metadata.rb
@@ -5,6 +5,10 @@ license          "Apache v2.0"
 description      "Configures sysctl parameters"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.3.2"
-%w(ubuntu debian redhat centos).each do |os|
+supports 'ubuntu', ">= 10.04"
+supports 'centos', ">= 5.9"
+%w(debian redhat).each do |os|
   supports os
 end
+conflicts "jn_sysctl"
+conflicts "el-sysctl"
