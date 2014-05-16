cdd39924b86d4b566a0f74118bb3a6dcca67d74a
Sander van Zoest <svanzoest@onehealth.com>
2014-05-16 11:11:16 -0700

    Version bump to 0.5.5 (for development)

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@onehealth.com'
 license 'Apache v2.0'
 description 'Configures sysctl parameters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.5.4'
+version '0.5.5'
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '>= 7.0'
 supports 'centos', '>= 5.9'
