51c216c3cba195bfe51ec7e98b1a88f5ebc96f9d
Sander van Zoest <sander@vanzoest.com>
2014-12-06 17:37:10 -0800

    Releaes v0.6.1

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'cookbooks@viveraehealth.com'
 license 'Apache v2.0'
 description 'Configures sysctl parameters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.6.0'
+version '0.6.1'
 supports 'ubuntu', '>= 12.04'
 supports 'debian', '>= 7.0'
 supports 'centos', '>= 5.9'
