db780b2bd609ece7b86106879cdc1cf0b45ac471
Sander van Zoest <sander.vanzoest@viverae.com>
2015-01-19 10:05:31 -0800

    Added support for ChefSpec < 4.1.0

--- libraries/matchers.rb
+++ libraries/matchers.rb
@@ -1,14 +1,11 @@
 if defined?(ChefSpec)
-  ChefSpec.define_matcher :sysctl_param
+  chefspec_version = Gem.loaded_specs['chefspec'].version
+    if chefspec_version < Gem::Version.new('4.1.0')
+      ChefSpec::Runner.define_runner_method :sysctl_param
+    else
+      ChefSpec.define_matcher :sysctl_param
+  end
 
-  # @example This is an example
-  # expect(chef_run.to apply_sysctl_param('foo')
-  #
-  # @param [String] resource_name
-  # the resource name
-  #
-  # @return [ChefSpec::Matchers::ResourceMatcher]
-  #
   def apply_sysctl_param(resource_name)
     ChefSpec::Matchers::ResourceMatcher.new(:sysctl_param, :apply, resource_name)
   end
