e5e190b9a436ea950520ba99263105fc271b1ee7
Sander van Zoest <svanzoest@wildfire.life>
2015-12-03 13:22:20 -0800

    Fix ohai spec bug in #38

--- test/integration/default/serverspec/ohai_plugin_spec.rb
+++ test/integration/default/serverspec/ohai_plugin_spec.rb
@@ -2,5 +2,5 @@ require 'spec_helper'
 
 describe command('ohai -d /etc/chef/ohai_plugins sys') do
   its(:exit_status) { should eq 0 }
-  its(:stdout) { should match %r("net": {/) }
+  its(:stdout) { should match %r("net": {) }
 end
