efa967493fd41a75bae21b619adbf8df6f7058e9
Jun SAKATA <jun.bj141400@gmail.com>
2016-03-01 17:42:21 +0900

    fixed for rubocop

--- libraries/sysctl.rb
+++ libraries/sysctl.rb
@@ -2,28 +2,21 @@
 module Sysctl
   class << self
     def config_file(node)
-      if node['sysctl']['conf_dir']
-        return node['sysctl']['conf_file']
-      else
-        if node['sysctl']['allow_sysctl_conf']
-          return node['sysctl']['conf_file']
-        else
-          return nil
-        end
-      end
+      return node['sysctl']['conf_file'] if node['sysctl']['conf_dir'] || node['sysctl']['allow_sysctl_conf']
+      nil
     end
 
     def compile_attr(prefix, v)
       case v
       when Array
-        return "#{prefix}=#{v.join(' ')}"
+        "#{prefix}=#{v.join(' ')}"
       when String, Fixnum, Bignum, Float, Symbol
         "#{prefix}=#{v}"
       when Hash, Chef::Node::Attribute
         prefix += '.' unless prefix.empty?
-        return v.map { |key, value| compile_attr("#{prefix}#{key}", value) }.flatten.sort
+        v.map { |key, value| compile_attr("#{prefix}#{key}", value) }.flatten.sort
       else
-        fail Chef::Exceptions::UnsupportedAction, "Sysctl cookbook can't handle values of type: #{v.class}"
+        raise Chef::Exceptions::UnsupportedAction, "Sysctl cookbook can't handle values of type: #{v.class}"
       end
     end
   end
