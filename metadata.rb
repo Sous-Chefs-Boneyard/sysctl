8a34619ee18ad9ce84edc9e2cdf7e6487a5f636e
Sander van Zoest <sander@vanzoest.com>
2016-11-18 13:29:22 -0800

    chef-brigade -> sous-chefs

--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name 'sysctl'
 maintainer 'Chef Brigade'
 maintainer_email 'help@chefbrigade.io'
-issues_url 'https://github.com/chef-brigade/sysctl/issues' if respond_to?(:issues_url)
-source_url 'https://github.com/chef-brigade/sysctl/' if respond_to?(:source_url)
+issues_url 'https://github.com/sous-chefs/sysctl/issues' if respond_to?(:issues_url)
+source_url 'https://github.com/sous-chefs/sysctl/' if respond_to?(:source_url)
 license 'Apache v2.0'
 description 'Configures sysctl parameters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
