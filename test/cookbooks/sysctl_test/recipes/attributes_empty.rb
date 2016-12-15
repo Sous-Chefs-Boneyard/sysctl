442ab195734fd366240ca3b4b7f911909ab71f75
Chase Bolt <chase.bolt@gmail.com>
2016-12-15 15:11:07 -0800

    adding tests for empty attribute case

--- test/cookbooks/sysctl_test/recipes/attributes_empty.rb
+++ test/cookbooks/sysctl_test/recipes/attributes_empty.rb
@@ -1,5 +1,7 @@
 ::Chef::Recipe.send(:include, SysctlCookbook::SysctlHelpers::Param)
 
+node.rm('sysctl')
+
 coerce_attributes(node['sysctl']['params']).each do |x|
   k, v = x.split('=')
   sysctl_param k do
