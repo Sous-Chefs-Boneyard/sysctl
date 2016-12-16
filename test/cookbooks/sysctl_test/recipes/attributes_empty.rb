d6e054e54b2d11220b8835499ca33d795e4dbdd4
Chase Bolt <chase.bolt@gmail.com>
2016-12-16 09:49:01 -0800

    dont dupe the resource for tests

--- test/cookbooks/sysctl_test/recipes/attributes_empty.rb
+++ test/cookbooks/sysctl_test/recipes/attributes_empty.rb
@@ -1,10 +1,3 @@
-::Chef::Recipe.send(:include, SysctlCookbook::SysctlHelpers::Param)
-
 node.rm('sysctl')
 
-coerce_attributes(node['sysctl']['params']).each do |x|
-  k, v = x.split('=')
-  sysctl_param k do
-    value v
-  end
-end if node.attribute?('sysctl') && node['sysctl'].attribute?('params')
+include_recipe 'sysctl_test::attributes'
