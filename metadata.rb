b17054570edc1e05fb0289397fdc904161411282
Dan Webb <dan.webb@damacus.io>
2017-02-28 20:07:44 +0000

    Fixing Sous Chefs Foodcritic warnings

--- metadata.rb
+++ metadata.rb
@@ -2,7 +2,7 @@ name 'sysctl'
 maintainer 'Sous Chefs'
 maintainer_email 'help@sous-chefs.org'
 issues_url 'https://github.com/sous-chefs/sysctl/issues'
-source_url 'https://github.com/sous-chefs/sysctl/'
+source_url 'https://github.com/sous-chefs/sysctl'
 license 'Apache v2.0'
 description 'Configures sysctl parameters'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
