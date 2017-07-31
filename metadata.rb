037ae341627ef4405d28e1f960f413ff1da82497
Tim Smith <tsmith@chef.io>
2017-07-31 14:32:50 -0700

    Add support for Amazon Linux on Chef 13
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -9,6 +9,7 @@ long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version '0.9.0'
 chef_version '>= 12.5' if respond_to?(:chef_version)
 
+supports 'amazon'
 supports 'ubuntu', '>= 14.04'
 supports 'debian', '>= 7.0'
 supports 'centos', '>= 6.0'
