e0874615b67c73f7f2aa536564eb3e6dffe5b8ac
Dan Webb <dan.webb@damacus.io>
2018-02-05 22:02:35 +0000

    Remove mentions of attributes

--- test/integration/attributes/inspec/assert_functioning_spec.rb
+++
@@ -1,9 +0,0 @@
-describe command('sysctl -n vm.swappiness') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/^19$/) }
-end
-
-describe file('/etc/sysctl.d/99-chef-vm.swappiness.conf') do
-  it { should be_file }
-  its(:content) { should match /^vm.swappiness = 19$/ }
-end
