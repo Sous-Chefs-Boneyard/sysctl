454f0042fa41e390069f30380571a8e6e5c54fc6
Dan Webb <dan.webb@damacus.io>
2018-02-05 22:42:38 +0000

    Add another test for removal

--- test/cookbooks/test/recipes/resources.rb
+++ test/cookbooks/test/recipes/resources.rb
@@ -20,3 +20,11 @@
 sysctl_param 'vm.swappiness' do
   value 19
 end
+
+sysctl_param 'kernel.msgmax' do
+  value 9000
+end
+
+sysctl_param 'kernel.msgmax' do
+  action :remove
+end
--- test/integration/resources/inspec/assert_functioning_spec.rb
+++ test/integration/resources/inspec/assert_functioning_spec.rb
@@ -1,9 +1,22 @@
-describe command('sysctl -n vm.swappiness') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/^19$/) }
+context 'kernel.msgmax' do
+  describe command('sysctl -n kernel.msgmax') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match(/^8192$/) }
+  end
+
+  describe file('/etc/sysctl.d/99-chef-kernel.msgmax.conf') do
+    it { should_not be_file }
+  end
 end
 
-describe file('/etc/sysctl.d/99-chef-vm.swappiness.conf') do
-  it { should be_file }
-  its(:content) { should match /^vm.swappiness = 19$/ }
+context 'vm.swappiness' do
+  describe file('/etc/sysctl.d/99-chef-vm.swappiness.conf') do
+    it { should be_file }
+    its(:content) { should match /^vm.swappiness = 19$/ }
+  end
+
+  describe command('sysctl -n vm.swappiness') do
+    its(:exit_status) { should eq 0 }
+    its(:stdout) { should match(/^19$/) }
+  end
 end
