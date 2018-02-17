bacc84a2dd806a673b457d063b74224056e529f1
Tim Smith <tsmith@chef.io>
2018-02-17 07:56:50 -0800

    Remove the unused helper methods
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -12,14 +12,6 @@ module SysctlCookbook
         o.stdout.to_s.tr("\t", ' ').strip
       end
 
-      def get_sysctld_value(key)
-        raise unless ::File.exist?("/etc/sysctl.d/99-chef-#{key}.conf")
-        k, v = IO.read("/etc/sysctl.d/99-chef-#{key}.conf").match(/(.*) = (.*)/).captures
-        raise 'Unknown sysctl key!' if k.nil?
-        raise 'Unknown sysctl value!' if v.nil?
-        v
-      end
-
       def coerce_value(v)
         case v
         when Array
@@ -29,42 +21,6 @@ module SysctlCookbook
         else
           v
         end
-      end
-
-      def service_type
-        s = {
-          name: nil,
-          provider: nil,
-        }
-        case node['platform']
-        when 'freebsd'
-          s['name'] = 'sysctl'
-        when 'arch', 'exherbo'
-          s['name'] = 'systemd-sysctl'
-          s['provider'] = Chef::Provider::Service::Systemd
-        when 'centos', 'redhat', 'scientific', 'oracle'
-          if node['platform_version'].to_f >= 7.0
-            s['name'] = 'systemd-sysctl'
-            s['provider'] = Chef::Provider::Service::Systemd
-          end
-        when 'fedora'
-          s['name'] = 'systemd-sysctl'
-          s['provider'] = Chef::Provider::Service::Systemd
-        when 'ubuntu'
-          if node['platform_version'].to_f < 15.04
-            s['provider'] = Chef::Provider::Service::Upstart
-          elsif node['platform_version'].to_f >= 15.04
-            s['provider'] = Chef::Provider::Service::Init::Systemd
-          end
-        when 'suse', 'opensuseleap'
-          if node['platform_version'].to_f < 12.0
-            s['name'] = 'boot.sysctl'
-          elsif node['platform_version'].to_f >= 12.0
-            s['name'] = 'systemd-sysctl'
-            s['provider'] = Chef::Provider::Service::Systemd
-          end
-        end
-        s
       end
     end
   end
