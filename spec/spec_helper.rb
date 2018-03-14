7ae3fa4d0e8aa621342ea6ef407683db2a49de88
Tim Smith <tsmith@chef.io>
2018-03-13 19:54:48 -0700

    Remove the unused method in the chefspec helper
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -7,15 +7,3 @@ RSpec.configure do |config|
   config.filter_run focus: true
   config.run_all_when_everything_filtered = true
 end
-
-def platforms
-  {
-    'ubuntu' => ['14.04', '16.04'],
-    'debian' => ['7.11', '8.8'],
-    'fedora' => ['25'],
-    'redhat' => ['6.9', '7.3'],
-    'centos' => ['6.9', '7.3.1611'],
-    'freebsd' => ['11.0'],
-    'suse' => ['12.2'],
-  }
-end
