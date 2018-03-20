ebd563fe2baedf09f6e5199f3d844aa581950949
Tim Smith <tsmith@chef.io>
2018-03-20 14:24:51 -0700

    Use the kernel_parameter inspec resource
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- test/integration/default/inspec/default_spec.rb
+++ test/integration/default/inspec/default_spec.rb
@@ -1,15 +1,13 @@
 if (os[:family] == 'redhat' && os[:release].start_with?('6')) || os[:name] == 'amazon' || os[:family] == 'suse'
 
-  describe command('sysctl -n kernel.msgmax') do
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should match(/^65536$/) }
+  describe kernel_parameter('kernel.msgmax') do
+    its('value') { should eq 65536 }
   end
 
 else
 
-  describe command('sysctl -n kernel.msgmax') do
-    its(:exit_status) { should eq 0 }
-    its(:stdout) { should match(/^8192$/) }
+  describe kernel_parameter('kernel.msgmax') do
+    its('value') { should eq 8192 }
   end
 
 end
@@ -18,7 +16,6 @@ describe file('/etc/sysctl.d/99-chef-kernel.msgmax.conf') do
   it { should_not be_file }
 end
 
-describe command('sysctl -n vm.swappiness') do
-  its(:exit_status) { should eq 0 }
-  its(:stdout) { should match(/^19$/) }
+describe kernel_parameter('vm.swappiness') do
+  its('value') { should eq 19 }
 end
